92bf104c290ae2215267426725079b7613981830
Erik Olofsson <playde5555@gmail.com>
2024-10-22 23:11:13 +0200

    Can search and find relevant text and highlight it

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,50 @@
-// The text you're searching for
-const searchText = "specific text";
+function searchPage() {
+  const cards = document.getElementsByClassName("Card_hclCard__v27k7");
+  
+  for (const card of cards) {
 
-// Check if the text is present in the body of the page
-// TODO Highlight the matched cards
-if (document.body.innerText.includes(searchText)) {
-  console.log(`The text "${searchText}" was found on this page!`);
-  alert(`The text "${searchText}" was found!`);
-} else {
-  console.log(`The text "${searchText}" was NOT found on this page.`);
+    const p = card.getElementsByTagName("p");
+
+    if (p.length == 0) 
+      continue; // No text, skip card.
+
+    const text = p[0].textContent;
+
+    if (classifyApartmentRenovation(text)) {
+      const div = card.firstElementChild;
+      div.style.border = "5px solid red"; 
+    }
+  }
 }
+
+
+function classifyApartmentRenovation(description) {
+  const needsRenovationKeywords = [
+    "sliten", "omodern", "behöver renoveras",
+    "totalrenovering krävs", "i behov av uppfräschning",
+    "slitage", "ytskikt behöver åtgärdas", "behov av upprustning", "ej renoverad",
+    "omfattande renovering(?!ar)",
+    "behöver uppdateras", "i behov av modernisering",
+    "äldre standard", "behöver fixas till",
+    "i behov av stambyte", "byggnadsår utan större renovering",
+    "behov av renovering", "renoveringschans",
+    "renoveringspotential", "renoveringsbehov",
+    "renoveringsobjekt", "renoveringsdröm"
+  ];
+
+  const needsRenovationKeywordsEscaped = needsRenovationKeywords.map(keyword =>
+    keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // Escape special characters
+  );
+
+  // Create the regex pattern by combining the keywords
+  const needsRenovationPattern = new RegExp(
+    "(" + needsRenovationKeywordsEscaped.join("|") + ")", 
+    "i" // Case insensitive flag
+  );
+
+  // Return true if any of the keywords are found in the description
+  return needsRenovationPattern.test(description);
+}
+
+
+searchPage();
